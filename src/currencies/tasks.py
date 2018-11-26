f0b885f2fd7271096f401b6c52d02d0e27cb039a
Dawid Bugajewski <buugaj@gmail.com>
2018-11-26 20:37:11 +0100

    Update django version, use generated code from Django 2.1

--- src/currencies/tasks.py
+++ src/currencies/tasks.py
@@ -1,13 +1,13 @@
 from __future__ import absolute_import, unicode_literals
 import logging
-
+from celery import task
 from django.conf import settings
-from mydjango.celery import app
+
 
 logger = logging.getLogger("celery")
 
 
-@app.task
+@task
 def show_hello_world():
     logger.info("-"*25)
     logger.info("Printing Hello from Celery")
--- src/currencies/views.py
+++ src/currencies/views.py
@@ -1,11 +1,3 @@
-from django.views.generic import TemplateView
-from .tasks import show_hello_world
+from django.shortcuts import render
 
 # Create your views here.
-
-class ShowHelloWorld(TemplateView):
-    template_name='hello_world.html'
-
-    def get(self, *args, **kwargs):
-        show_hello_world.apply()
-        return super().get(*args, **kwargs)
