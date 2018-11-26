1f059b6692b2ac12a4e8e3501c2db46880b7d9c7
Dawid Bugajewski <buugaj@gmail.com>
2018-11-27 00:28:04 +0100

    Fix currencies serializer, add latest currencies view

--- src/currencies/serializers.py
+++ src/currencies/serializers.py
@@ -2,7 +2,7 @@ from currencies.models import ExchangeRate
 from rest_framework import serializers
 
 
-class ExchangeRateSerializer(serializers.HyperlinkedModelSerializer):
+class ExchangeRateSerializer(serializers.ModelSerializer):
     class Meta:
         model = ExchangeRate
-    fields = ('date', 'base_currency','target_currency','rate')
+        fields = ('date', 'base_currency','target_currency','rate')
--- src/currencies/views.py
+++ src/currencies/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets, pagination
 from currencies.models import ExchangeRate
 from currencies.serializers import ExchangeRateSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 class StandardResultsSetPagination(pagination.PageNumberPagination):
     page_size = 100
@@ -11,6 +13,12 @@ class ExchangeRateViewSet(viewsets.ReadOnlyModelViewSet):
     """
     API endpoint that allows groups to be viewed or edited.
     """
-    queryset = ExchangeRate.objects.all()
+    queryset = ExchangeRate.objects.all().order_by('-date')
     serializer_class = ExchangeRateSerializer
     pagination_class = StandardResultsSetPagination
+
+    @action(detail=False)
+    def latest(self, request):
+        current =  ExchangeRate.objects.all().order_by('base_currency', 'target_currency', '-date', 'rate').distinct('base_currency','target_currency')
+        serializer = self.get_serializer(current, many=True)
+        return Response(serializer.data)
