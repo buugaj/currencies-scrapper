b3ba9168c599846548383b15960dd8100e2af180
Dawid Bugajewski <buugaj@gmail.com>
2018-11-26 21:13:58 +0100

    Add scrapper for currency rss, add libraries to parse xml and parse datetime

--- src/currencies/scrapper.py
+++ src/currencies/scrapper.py
@@ -1,10 +1,14 @@
 import requests
 import re
-
+from bs4 import BeautifulSoup
+from collections import namedtuple
+from decimal import Decimal
+from dateutil import parser
 rss_hostname = "https://www.ecb.europa.eu"
 rss_list_url = "https://www.ecb.europa.eu/home/html/rss.en.html"
 rss_url_pattern = re.compile(r'(\/rss\/fxref-[\w]+.html)')
 
+ScrapResult = namedtuple('ScrapResult','date base_currency target_currency exchange_rate')
 def scrap_rss_urls():
     request = requests.get(rss_list_url)
     request.raise_for_status()
@@ -13,4 +17,16 @@ def scrap_rss_urls():
     urls = [rss_hostname+x for x in matches]
     return urls
 
-scrap_rss_urls()
+def scrap_currency_rss(url):
+    request = requests.get(url)
+    request.raise_for_status()
+    response = request.content
+    soup = BeautifulSoup(response, "html.parser")
+    items = soup.find_all('item')
+    results = []
+    for item in items:
+        date = parser.parse(item.find('dc:date').text)
+        base_currency = item.find('cb:basecurrency').text
+        target_currency = item.find('cb:targetcurrency').text
+        exchange_rate =  Decimal(item.find('cb:value').text)
+        results.append(ScrapResult(date,base_currency,target_currency,exchange_rate))
