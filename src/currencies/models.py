14e02a89ff5420071b66a539a0c708430a72d84a
Dawid Bugajewski <buugaj@gmail.com>
2018-11-26 20:42:37 +0100

    Add currencies to installed_apps, define currency and exchange rate models

--- src/currencies/models.py
+++ src/currencies/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Currency(models.Model):
+    code = models.CharField(max_length=3, primary_key=True)
+
+class ExchangeRate(models.Model):
+    date = models.DateTimeField()
+    base_currency = models.ForeignKey(Currency,related_name='base_currency', on_delete=models.PROTECT)
+    target_currency = models.ForeignKey(Currency,related_name='target_currency', on_delete=models.PROTECT)
+    rate = models.DecimalField(max_digits=16,decimal_places=8)
+
+
