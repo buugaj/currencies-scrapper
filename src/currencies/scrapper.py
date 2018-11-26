ca69340ee41c2e91a8bf50ac9bacd0d94c58dd4a
Dawid Bugajewski <buugaj@gmail.com>
2018-11-27 00:35:51 +0100

    Add retrying to scrapper in case of unexpected errors

--- src/currencies/scrapper.py
+++ src/currencies/scrapper.py
@@ -4,19 +4,26 @@ from bs4 import BeautifulSoup
 from collections import namedtuple
 from decimal import Decimal
 from dateutil import parser
+from retrying import retry
+
 rss_hostname = "https://www.ecb.europa.eu"
 rss_list_url = "https://www.ecb.europa.eu/home/html/rss.en.html"
 rss_url_pattern = re.compile(r'(\/rss\/fxref-[\w]+.html)')
 
-ScrapResult = namedtuple('ScrapResult','date base_currency target_currency exchange_rate')
+ScrapResult = namedtuple('ScrapResult', 'date base_currency target_currency exchange_rate')
+
+
+@retry(wait_exponential_multiplier=1000, wait_exponential_max=10000)
 def scrap_rss_urls():
     request = requests.get(rss_list_url)
     request.raise_for_status()
     response = request.text
-    matches = re.findall(rss_url_pattern,response)
-    urls = [rss_hostname+x for x in matches]
+    matches = re.findall(rss_url_pattern, response)
+    urls = [rss_hostname + x for x in matches]
     return urls
 
+
+@retry(wait_exponential_multiplier=1000, wait_exponential_max=10000)
 def scrap_currency_rss(url):
     request = requests.get(url)
     request.raise_for_status()
@@ -28,6 +35,6 @@ def scrap_currency_rss(url):
         date = parser.parse(item.find('dc:date').text)
         base_currency = item.find('cb:basecurrency').text
         target_currency = item.find('cb:targetcurrency').text
-        exchange_rate =  Decimal(item.find('cb:value').text)
-        results.append(ScrapResult(date,base_currency,target_currency,exchange_rate))
+        exchange_rate = Decimal(item.find('cb:value').text)
+        results.append(ScrapResult(date, base_currency, target_currency, exchange_rate))
     return results
